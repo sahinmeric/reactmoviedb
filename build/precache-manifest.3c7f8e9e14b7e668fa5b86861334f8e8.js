cbc0d2ac3e5bb0dd436180a85c2139cb5906b532
sahinmeric <sahin.meric86@gmail.com>
2021-08-15 22:32:18 -0300

    first commit

---
+++ build/precache-manifest.3c7f8e9e14b7e668fa5b86861334f8e8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88c3efb957593f54a94e264628af03bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53552c9d385ac8ae0ec6",
+    "url": "/static/js/2.b9c7d76d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b9c7d76d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09b620717aed34629140",
+    "url": "/static/js/main.058b6ad8.chunk.js"
+  },
+  {
+    "revision": "83196afb054f26bfe2f3",
+    "url": "/static/js/runtime-main.49bd2b20.js"
+  },
+  {
+    "revision": "31f5bb1da5a478ef98a9213900c5f7a2",
+    "url": "/static/media/no_image.31f5bb1d.jpg"
+  },
+  {
+    "revision": "5de5919a93cba46a09c8df1296a094ee",
+    "url": "/static/media/react-movie-logo.5de5919a.svg"
+  },
+  {
+    "revision": "80e9d8a2ff7fddf4a6dc1309d9e8a3ff",
+    "url": "/static/media/search-icon.80e9d8a2.svg"
+  },
+  {
+    "revision": "30cd724bda43bdfa29216c8d8770b0c9",
+    "url": "/static/media/tmdb_logo.30cd724b.svg"
+  }
+]);
